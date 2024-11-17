c9ee68bb4f746223a47b24012552242f93d50498
OpenOI-Dev team <144697157+xyt2013@users.noreply.github.com>
2024-11-17 19:20:53 +0800

    Add files via upload

---
+++ ddos/ddos.h
@@ -0,0 +1,16 @@
+﻿// ddos.h: 标准系统包含文件的包含文件
+// 或项目特定的包含文件。
+
+#pragma once
+
+#ifndef UNICODE
+#define UNICODE
+#endif
+
+#define WIN32_LEAN_AND_MEAN
+
+#include <windows.h>
+#include <winsock2.h>
+#include <Ws2tcpip.h>
+#include <stdio.h>
+#include <stdlib.h>
